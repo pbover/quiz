1136cf3f55efb453feb7af1cdc46864e14a2b91e
Ponç Bover <pobover@gmail.com>
2015-05-30 01:45:29 +0200

    Marco de aplicación + css
    Añadidos layout y ficheros de estilos para responsividad de la página

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
+var partials = require('express-partials');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
