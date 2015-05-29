b6d699bc43a87cdcd200b727fbaa775814468896
Ponç Bover <pobover@gmail.com>
2015-05-30 01:15:25 +0200

    Primera pregunta
    Primera pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,6 @@ exports.question = function(req,res)
 {
 res.render('quizes/question',{pregunta:'Capital de España?',pregunta2:'Capital de Francia?'});
 };
-
 exports.answer = function(req,res)
 {
 	//if(req.query.respuesta ==='Madrid' && req.query.respuesta2 ==='Paris')
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 var quizController = require("../controllers/quiz_controller");
 /* GET home page. */
 router.get('/', function(req, res) {
