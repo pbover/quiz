5b092e87b9e4d87ffc499219bfe3d90e92577085
Ponç Bover <pobover@gmail.com>
2015-05-30 19:37:33 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,12 @@ router.get('/', function(req, res) {
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos del ejercicio' });
 });
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+
+router.get('/quizes',          quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
 
 
 module.exports = router;
